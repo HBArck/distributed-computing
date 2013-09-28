733074024eed1200775628def6b612d045e5660d
nborisov <nik.born@gmail.com>
2013-09-28 19:29:46 +0300

    Merge branch 'master' of https://github.com/bornik/distributed-computing
    Conflicts:
    	public/index.html

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var express = require("express");
 var app = express();
 var path = require('path'); // модуль для парсинга пути
 var http = require ('http');             // For serving a basic web page.
-var mongoose = require ("mongoose"); // The reason for this demo.
+//var mongoose = require ("mongoose"); // The reason for this demo.
 
 app.use(express.logger());
 app.use(express.static(path.join(__dirname, "public")));
@@ -24,23 +24,23 @@ var mongoUri = process.env.MONGOLAB_URI ||
     process.env.MONGOHQ_URL ||
     'mongodb://herokumongo:_mongopass_123@paulo.mongohq.com:10011/app18351472';
 
-//console.log('>>>>>', mongoUri);
-//mongo.Db.connect(mongoUri, function (err, db) {
-//    if (err) {
-////        sys.puts(err);
-//        console.log('++++++++++Error mongo connection++++++++', err);
-//    } else {
-//        db.collection('mydocs', function(er, collection) {
-//            collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
-//            });
-//        });
-//    }
-//});
-
-mongoose.connect(mongoUri, function (err, res) {
+console.log('>>>>>', mongoUri);
+mongo.Db.connect(mongoUri, function (err, db) {
     if (err) {
-        console.log ('ERROR connecting to: ' + mongoUri + '. ' + err);
+//        sys.puts(err);
+        console.log('++++++++++Error mongo connection++++++++', err);
     } else {
-        console.log ('Succeeded connected to: ' + mongoUri);
+        db.collection('mydocs', function(er, collection) {
+            collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+            });
+        });
     }
 });
+
+//mongoose.connect(mongoUri, function (err, res) {
+//    if (err) {
+//        console.log ('ERROR connecting to: ' + mongoUri + '. ' + err);
+//    } else {
+//        console.log ('Succeeded connected to: ' + mongoUri);
+//    }
+//});
