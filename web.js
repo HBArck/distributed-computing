f5c5b10a91b95ce4df84a738f033eb3fd1028118
nborisov <nik.born@gmail.com>
2013-09-28 19:40:27 +0300

    Merge branch 'master' of https://github.com/bornik/distributed-computing
    Conflicts:
    	public/index.html

--- web.js
+++ web.js
@@ -3,12 +3,12 @@
  */
 var express = require("express");
 var app = express();
-var path = require('path'); // модуль для парсинга пути
+//var path = require('path'); // модуль для парсинга пути
 //var http = require ('http');             // For serving a basic web page.
 //var mongoose = require ("mongoose"); // The reason for this demo.
 
 app.use(express.logger());
-app.use(express.static(path.join(__dirname, "public")));
+//app.use(express.static(path.join(__dirname, "public")));
 app.get('/hello', function(request, response) {
     response.send('Hello World11!');
 });
@@ -18,24 +18,24 @@ app.listen(port, function() {
     console.log("Listening on " + port);
 });
 
-var mongo = require('mongodb');
-
-var mongoUri = process.env.MONGOLAB_URI ||
-    process.env.MONGOHQ_URL ||
-    'mongodb://herokumongo:_mongopass_123@paulo.mongohq.com:10011/app18351472';
-
-console.log('>>>>>', mongoUri);
-mongo.Db.connect(mongoUri, function (err, db) {
-    if (err) {
-//        sys.puts(err);
-        console.log('++++++++++Error mongo connection++++++++', err);
-    } else {
-        db.collection('mydocs', function(er, collection) {
-            collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
-            });
-        });
-    }
-});
+//var mongo = require('mongodb');
+//
+//var mongoUri = process.env.MONGOLAB_URI ||
+//    process.env.MONGOHQ_URL ||
+//    'mongodb://herokumongo:_mongopass_123@paulo.mongohq.com:10011/app18351472';
+//
+//console.log('>>>>>', mongoUri);
+//mongo.Db.connect(mongoUri, function (err, db) {
+//    if (err) {
+////        sys.puts(err);
+//        console.log('++++++++++Error mongo connection++++++++', err);
+//    } else {
+//        db.collection('mydocs', function(er, collection) {
+//            collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+//            });
+//        });
+//    }
+//});
 
 //mongoose.connect(mongoUri, function (err, res) {
 //    if (err) {
